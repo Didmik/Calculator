595142f8a17001304f53502c94c30b7f6d2c692b
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-05-17 13:12:03 +0200

    Added alle files and a container for the calculator

---
+++ script.js
@@ -0,0 +1,43 @@
+let num = 0;
+let operator = "";
+let num2 = 0;
+
+
+function operate(num, operator, num2) {
+
+}
+
+
+
+const add = function(num, num2) {
+	if (isNaN(num) || isNaN(num2)) {
+    return "is not a number";
+  }
+
+  else {
+    return num + num2;
+  }
+ 
+};
+
+const subtract = function(num, num2) {
+    return num - num2;
+};
+
+
+
+const multiply = function(num, num2) {
+    /*let sum = 1;
+    for (let i = 0; i <= args.length; i++) {
+      sum *= args[i];
+    }
+    return sum; */
+  
+    return num * num2;
+ }
+
+
+
+ const divide = function(num, num2) {
+    return num / num2;
+};
