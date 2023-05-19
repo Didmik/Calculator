e53830c67553b83886e78421216036c84bdd8279
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-05-19 14:49:52 +0200

    Added disabled buttons funvtion after equals and activated button function after clear

--- script.js
+++ script.js
@@ -1,24 +1,130 @@
-let num = 0;
-let operator = "";
-let num2 = 0;
+let num = [];
+let operator = [];
+let num2 = [];
+let displayValue = [];
 
 
-function operate(num, operator, num2) {
-   operate
+// Display
+
+let display = document.querySelector(".display");
+
+
+// Buttons
+
+let buttons = document.querySelectorAll("button");
+
+let numbers = document.querySelectorAll(".number");
+
+let operators = document.querySelectorAll(".operator");
+
+let clear = document.querySelector(".clear");
+
+let equals = document.querySelector(".equals");
+
+
+
+/*buttons.forEach(number => {
+  numbers.addEventListener('click', function(){
+    display.textContent = button.value;
+  })
+}) */
+
+
+
+numbers.forEach(numb => {
+  numb.addEventListener('click', function(e) {
+    if (operator === "") { // Read first number if no operator set yet
+      num += e.target.innerText;
+      //num.slice(1);
+      display.textContent = numb.value;
+     // displayValue.push(num)
+      console.log(num)
+    } 
+    else { // Read second number
+      num2 += e.target.innerText;
+      display.textContent = numb.value;
+     // displayValue.push(num);
+      console.log(num2)
+    } 
+  })
+})
+
+operators.forEach(oper => {
+  oper.addEventListener("click", e => {
+      operator = e.target.innerText;
+      display.textContent = oper.value;
+      console.log(operator)
+      // If equals operator, perform operation
+
+      switch (operator) { // Calculate and print output
+        case "+":
+            console.log(num + num2);
+            break;
+
+        case "-":
+            console.log(num - num2);
+            break;
+
+        case "*":
+            console.log(parseInt(num) * parseInt(num2));
+            break;
+        
+        case "/":
+            console.log(parseInt(num) / parseInt(num2));
+            break;
+
+        default:
+            break;
+    }
+  });
+});
+
+
+
+function disableButtons() {
+  buttons.forEach(cli => {
+    if (cli !== clear) { 
+    cli.disabled = true;
+   }
+  })
+}
+
+
+function activateButtons() {
+  buttons.forEach(cli => {
+    cli.disabled = false;
+  })
 }
 
 
 
-const add = function(num, num2) {
-	if (isNaN(num) || isNaN(num2)) {
-    return "is not a number";
-  }
+// clear function
+clear.addEventListener("click", function() {
+  display.textContent = "";
+  num = [];
+  num2 = [];
+  operator = [];
+  activateButtons();
+})
 
-  else {
+
+// equals function
+equals.addEventListener("click", function() {
+ console.log(  operate(num, operator, num2)  )
+ disableButtons();
+  
+  //display.textContent = numb.value;
+})
+
+
+
+
+
+
+const add = function(num, num2) {
     return num + num2;
   }
- 
-};
+
 
 const subtract = function(num, num2) {
     return num - num2;
@@ -27,12 +133,6 @@ const subtract = function(num, num2) {
 
 
 const multiply = function(num, num2) {
-    /*let sum = 1;
-    for (let i = 0; i <= args.length; i++) {
-      sum *= args[i];
-    }
-    return sum; */
-  
     return num * num2;
  }
 
@@ -41,3 +141,12 @@ const multiply = function(num, num2) {
  const divide = function(num, num2) {
     return num / num2;
 };
+
+
+
+function operate(num, operator, num2) {
+  //console.log (divide(1, 2) );
+}
+
+operate()
+
